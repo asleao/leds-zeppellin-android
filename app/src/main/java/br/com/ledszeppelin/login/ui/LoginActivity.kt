4b6a94e0622b85a61e31dce169811630291d472c
André Spadetto Leão <andre.sp.leao@gmail.com>
2019-07-07 18:15:23 -0300

    Configuring navigation.

---
+++ app/src/main/java/br/com/ledszeppelin/login/ui/LoginActivity.kt
@@ -0,0 +1,52 @@
+package br.com.ledszeppelin.login.ui
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import br.com.ledszeppelin.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fg_login_navhost) as NavHostFragment?
+            ?: return
+
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBar(navController, appBarConfiguration)
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfig: AppBarConfiguration
+    ) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.fg_login_navhost).navigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.fg_login_navhost))
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        LoginRepository.destroyInstance()
+//        LoginRemoteDataSource.destroyInstance()
+    }
+}
